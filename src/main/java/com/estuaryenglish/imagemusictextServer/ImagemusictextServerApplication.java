c4f05ece6579d1ddc33f8d8e6f32a2e7c875d46f
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2023-05-01 10:25:12 +0100

    Configured project so server can run with temp DB.

--- src/main/java/com/estuaryenglish/imagemusictextServer/ImagemusictextServerApplication.java
+++ src/main/java/com/estuaryenglish/imagemusictextServer/ImagemusictextServerApplication.java
@@ -2,12 +2,19 @@ package com.estuaryenglish.imagemusictextServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ImagemusictextServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImagemusictextServerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
 }
