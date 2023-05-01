4364b461cf57c407f94337cf919bf89fa7a2580b
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2023-05-01 11:15:50 +0100

    Added ProjectsControllerIntegrationTest with basic check for empty db.

---
+++ src/test/java/com/estuaryenglish/imagemusictextServer/project/ProjectsControllerIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.estuaryenglish.imagemusictextServer.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ProjectsControllerIntegrationTest {
+    @Autowired private MockMvc mvc;
+    @Autowired private ProjectsRepository repository;
+    @Autowired private ProjectsService service;
+
+    @Test
+    void whenNoProjectsSaved_indexReturnsEmptyIterable() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/projects").contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$", hasSize(0)));
+    }
+}
